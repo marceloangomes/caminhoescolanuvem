98122e01ae44cb7747f123d80ffa74dfca8c9da2
marceloangomes <marcelogomes723@gmail.com>
2023-08-16 20:56:02 -0300

    Reestruturação - remoção bootstrap

--- www/App/Component/Template/schoolHead.js
+++ www/App/Component/Template/schoolHead.js
@@ -1,16 +1,14 @@
 export { SchoolHeadTemplate };
 
-const SchoolHeadTemplate = (el,i) => {
+const SchoolHeadTemplate = (el, i) => {
   let elChild = document.createElement('div');
-  if(i==0)
-    el.innerHTML = `<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist"></ul>`;
-  elChild.innerHTML = `    
-                  <li class='nav-item'>
-                    <a class='nav-link' id='pills' data-toggle='pill' href='#pills' role='tab' aria-controls='pills'
-                      aria-selected='true' style='display:none;'></a>
-                  </li>                     
+  if (i == 0)
+    el.innerHTML = `<div class="nav-pills-container"></div>`;
+  elChild.innerHTML = `                      
+                      <a class='nav-pills' id='pills' data-toggle='pill' href='#pills' role='tab' aria-controls='pills'
+                        aria-selected='true' style='display:none;'></a>                                        
                 `;
-  el.querySelector('ul').appendChild(elChild.querySelector('li'));
+  el.querySelector('.nav-pills-container').appendChild(elChild.querySelector('a'));
   return el;
 }
 
