c307e47386563ae9b2a6a5e83f03cbdcf24c7e4f
marceloangomes <marcelogomes723@gmail.com>
2023-08-24 19:44:51 -0300

    Migração Node

--- server/App/app.js
+++ server/App/app.js
@@ -1,6 +1,6 @@
+
 import { CacheNodeFacade } from './Data/CacheNodeFacade.js';
 import {IsValidCacheImplementation} from './Data/CacheInterface.js';
-import { GetData } from './Data/Data.js'
 import http from 'http';
 
 const hostname = '127.0.0.1';
@@ -9,12 +9,9 @@ const port = 3000;
 const server = http.createServer(async (req, res) => {
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
-    let cache = new CacheNodeFacade()
-    if(IsValidCacheImplementation(cache)){
-        const data = await GetData(cache);    
+    
         res.end(JSON.stringify(data));
-    }else
-        throw Error('Erro no componente: CacheNodeFacade');
+    
 })
 
 server.listen(port, hostname, () => {
---
+++ server/App/main.js
@@ -0,0 +1,254 @@
+import { Sleep, AsyncForEach } from './Library.js';
+import { GetData } from './Data.js';
+import { SchoolHead } from './Component/schoolHead.js'
+import { SchoolClose } from './Component/schoolClose.js';
+import { SchoolNeighbor } from './Component/schoolNeighbor.js';
+import { FactoryComponent } from './Component/factoryComponent.js';
+import { Map } from './Component/map.js';
+import { GetData } from './Data/Data.js';
+export { Update } from './main.js';
+
+"use strict";
+
+const InitMap = async () => {
+    const { Map } = await google.maps.importLibrary("maps");
+    const { Geometry } = await google.maps.importLibrary("geometry");
+    const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+}
+
+InitMap();
+
+const Update = async () => {
+    try {
+        let cache = new CacheNodeFacade();
+        if (!IsValidCacheImplementation(cache))
+            throw Error('Erro no componente: CacheNodeFacade');
+        const data = await GetData(cache);
+        filter.Init(data, year);
+        const schoolSelected = SchoolSelected(data, parseInt(selector('#selSchool').value));
+        const schoolsFiltered = ApplyFilter(filter, data, schoolSelected);
+
+        //POC
+        //filter.addressOrigin = 'lirio dos vales, 106, São Bernardo do Campo, SP'
+        //filter.addressOrigin = "R Simão da mata, 299, São Bernardo do Campo, SP"
+        //filter.addressOrigin = "Av Antonio Toneto, 105, São Bernardo do Campo, SP"
+        //filter.addressOrigin = "Rua Princesa Maria da Glória, 176, São Bernardo do Campo, SP"
+        //filter.addressOrigin = 'R. Me. de Deus, 263 - Mooca, São Paulo - SP, 03119-000'
+        //filter.addressOrigin = 'Av. Dona Ruyce Ferraz Alvim, 631 - Serraria, Diadema - SP, 09961-540';
+        //filter.addressOrigin = 'Av. Fundibem, 1010 - Casa Grande, Diadema - SP, 09961-390';
+        //filter.addressOrigin = 'Av. Humberto de Alencar Castelo Branco, 2563 - Chácara Dublin Paulista, São Bernardo do Campo - SP, 09851-320';
+
+        const locationOrigin = await AddressOriginToLocation(filter.addressOrigin, data.message);
+        const schoolsRay = FilterSchoolsByRay(locationOrigin, schoolsFiltered, data.message);
+        if (!schoolsRay)
+            throw (new Error(data.message.noFindedSchool));
+        let wayCloses = await ProcessSchoolsRay(locationOrigin, schoolsRay);
+        if (!wayCloses)
+            throw (new Error(data.message.noLocation));
+        wayCloses = SortWays(wayCloses);
+        return { "ways": FilterWays(wayCloses), "neighbors": FilterNeighbors(wayCloses) }
+    } catch (error) {
+        ShowAlert(error);
+    } finally {
+        Hide("#wait");
+    }
+}
+
+const SchoolSelected = (data, school_id) => {
+    let schoolFound = data.schools.find(school => school.selected);
+    if (schoolFound)
+        schoolFound.selected = false;
+    if (school_id === 0)
+        return false;
+    schoolFound = data.schools.find(school => school.school_id == school_id);
+    if (schoolFound) {
+        schoolFound.selected = true;
+        schoolFound.junctionsId = [];
+        return schoolFound;
+    }
+    throw Error(data.message.noFindedSchool);
+}
+
+const ApplyFilter = (filter, data, schoolSelected) => {
+    let schoolsFiltered = [];
+    if (schoolSelected)
+        schoolsFiltered.push(schoolSelected);
+    const junctionsId = [];
+    filter.parameters.forEach(parameter => {
+        data.junctions.filter(junction => {
+            return junction.model_id == parameter.model_id && junction.shift_id == parameter.shift_id && junction.year_id == parameter.year_id;
+        }).forEach(junction => {
+            junctionsId.push(junction.id);
+        })
+    })
+
+    data.schools.forEach(school => {
+        if (school.neighbor)
+            schoolsFiltered.push(school);
+        else {
+            const schoolJunction = data.schoolJunctions.find(schoolJunction => school.school_id == schoolJunction.school_id)
+            const junctionsIdFound = junctionsId.find(id => schoolJunction.junction.indexOf(id) > -1);
+            if (junctionsIdFound) {
+                const schoolFound = schoolsFiltered.find(schoolFiltered => school.school_id == schoolFiltered.school_id)
+                if (schoolFound) {
+                    if (!schoolFound.junctionsId)
+                        schoolFound.junctionsId = [];
+                    schoolFound.junctionsId.push(junctionsIdFound);
+                } else {
+                    let schoolFiltered = school;
+                    schoolFiltered.junctionsId = [];
+                    schoolFiltered.junctionsId.push(junctionsIdFound);
+                    schoolsFiltered.push(schoolFiltered);
+                }
+            }
+        }
+    })
+    return schoolsFiltered;
+}
+
+const ValidationSchoolsByRay = (school, locationOrigin, distanceRay) => {
+    if (school.lat && school.lng) {
+        let locationDestiny = new google.maps.LatLng(school.lat, school.lng);
+        let distance = google.maps.geometry.spherical.computeDistanceBetween(locationOrigin, locationDestiny);
+        return distance && distance <= distanceRay;
+    }
+    return false;
+};
+
+const DistanceSchoolsByRay = (school, locationOrigin) => {
+    if (school.lat && school.lng) {
+        let locationDestiny = new google.maps.LatLng(school.lat, school.lng);
+        let distance = google.maps.geometry.spherical.computeDistanceBetween(locationOrigin, locationDestiny);
+        return distance;
+    }
+    return false;
+};
+
+const FilterSchoolsByRay = (locationOrigin, schools) => {
+    let schoolsRay = schools;
+    let distanceRay = 2100;
+    do {
+        schoolsRay = schoolsRay.filter(school => school.selected || ValidationSchoolsByRay(school, locationOrigin, distanceRay));
+        distanceRay -= 10;
+    } while ((schoolsRay.filter(schoolRay => { return !schoolRay.neighbor && !schoolRay.selected }).length > 7) || distanceRay === 10);
+
+    const counterSchools = schoolsRay.filter(schoolRay => { return !schoolRay.neighbor && !schoolRay.selected }).length
+    if (counterSchools < 3) {
+        distanceRay += 10;
+        do {
+            distanceRay += 10;
+            schoolsRay = schools.filter(school => school.selected || ValidationSchoolsByRay(school, locationOrigin, distanceRay));
+        } while (schoolsRay.filter(schoolRay => { return !schoolRay.neighbor && !schoolRay.selected }).length < 3 || distanceRay === 15000);
+    };
+
+    schoolsRay.map(school => {
+        school.distanceRay = DistanceSchoolsByRay(school, locationOrigin);
+    });
+    let schoolsNeighbor = schoolsRay.filter(school => { return school.neighbor });
+    schoolsNeighbor.sort((a, b) => {
+        return a.distanceRay - b.distanceRay
+    });
+    schoolsNeighbor = schoolsNeighbor.slice(0, 3);
+    schoolsRay = schoolsRay.filter(school => { return !school.neighbor })
+    schoolsRay = schoolsRay.concat(schoolsNeighbor);
+    return schoolsRay;
+}
+
+const SortWays = (ways) => {
+    ways.sort((a, b) => {
+        if (a.school.selected < b.school.selected)
+            return 1;
+        else if (a.school.selected > b.school.selected)
+            return -1;
+        else
+            return a.distance - b.distance;
+    });
+    return ways;
+}
+
+const FilterWays = (ways) => {
+    const schoolSelected = ways.find(way => way.school.selected);
+    const wayVisions = ways.filter(way => { return !way.school.neighbor })
+        .slice(0, 3 + (schoolSelected ? 1 : 0));
+    wayVisions.map(way =>
+        way.addressDestiny += ' escola'
+    );
+    return wayVisions;
+}
+
+const FilterNeighbors = (ways) => {
+    return ways.filter(way => { return way.school.neighbor === true });
+}
+
+const GetDistanceMatrixPromise = (options) => {
+    const service = new google.maps.DistanceMatrixService();
+    return new Promise((resolve, reject) => {
+        service.getDistanceMatrix(options, (response, status) => {
+            if (status === google.maps.DistanceMatrixStatus.OK) {
+                resolve(response);
+            } else {
+                reject(new Error('Não foi possível calcular as distâncias, tente novamente.'));
+            }
+        });
+    });
+}
+
+const CalculateDistance = async (school, locationOrigin, locationDestiny) => {
+    const response = await GetDistanceMatrixPromise({
+        origins: [locationOrigin],
+        destinations: [locationDestiny],
+        travelMode: google.maps.TravelMode.WALKING, // Modo (DRIVING | WALKING | BICYCLING)
+        unitSystem: google.maps.UnitSystem.METRIC // Sistema de medida (METRIC | IMPERIAL)            
+    });
+    return { "school": school, "locationOrigin": locationOrigin, "locationDestiny": locationDestiny, "distance": response.rows[0].elements[0].distance.value, "addressDestiny": response.destinationAddresses, "addressOrigin": response.originAddresses, "distanceLong": response.rows[0].elements[0].distance.text, "time": response.rows[0].elements[0].duration.text };
+}
+
+const ProcessSchoolsRay = async (locationOrigin, schoolsRay) => {
+    let i = 0;
+    let ways = [];
+    await new Promise((resolve) => {
+        AsyncForEach(schoolsRay, async (school) => {
+            i++;
+            if (i % 4 === 0)
+                await Sleep(1010);
+            const locationDestiny = new google.maps.LatLng(school.lat, school.lng);
+            ways.push(await CalculateDistance(school, locationOrigin, locationDestiny));
+            if (i === schoolsRay.length)
+                resolve();
+        });
+    });
+    return ways;
+}
+
+const GeocodePromisse = (options, message) => {
+    let geocoder = new google.maps.Geocoder();
+    return new Promise((resolve, reject) => {
+        geocoder.geocode(options, (response, status) => {
+            if (status === google.maps.GeocoderStatus.OK)
+                resolve(response);
+            else if (status === google.maps.GeocoderStatus.ZERO_RESULTS)
+                reject(new Error(message.noFindedAddress));
+            else
+                reject(new Error('Não foi possível geolocalizar, tente novamente.'));
+        });
+    });
+};
+
+const AddressOriginToLocation = async (addressOrigin, message) => {
+    const response = await GeocodePromisse({ 'address': addressOrigin, 'region': 'BR' }, message);
+    const lat = response[0].geometry.location.lat();
+    const lng = response[0].geometry.location.lng();
+    return new google.maps.LatLng(lat, lng);
+}
+
+const CreateComponents = () => {
+    const schoolHead = new FactoryComponent('school-head', SchoolHead);
+    const schoolClose = new FactoryComponent('school-close', SchoolClose);
+    const schoolNeighbor = new FactoryComponent('school-neighbor', SchoolNeighbor);
+    const map = new FactoryComponent('map-close', Map);
+    return { 'schoolHead': schoolHead, 'schoolClose': schoolClose, 'schoolNeighbor': schoolNeighbor, 'map': map };
+}
+
+
+
+
